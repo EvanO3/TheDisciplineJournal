9f36a8a7c374a554976928b3ed77979c4dfc729c
”EvanO3” <evan.oni@hotmail.com>
2025-10-25 22:24:21 -0400

    retrieve user method

--- src/main/java/com/journal/discipline/tracker/Service/UserService.java
+++ src/main/java/com/journal/discipline/tracker/Service/UserService.java
@@ -1,8 +1,12 @@
 package com.journal.discipline.tracker.Service;
 
+import java.util.UUID;
+
 import com.journal.discipline.tracker.DTOs.UserDTO;
+import com.journal.discipline.tracker.DTOs.UserResponse;
 
 public interface UserService {
     
     UserDTO createUser(UserDTO user);
+    UserResponse retrieveUser(UUID userId);
 }
