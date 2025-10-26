5a13ae53b5f6fb65a34dd908dedb9c8fc48578e6
”EvanO3” <evan.oni@hotmail.com>
2025-10-25 22:06:54 -0400

    added timestamp for creation

--- src/main/java/com/journal/discipline/tracker/Model/User.java
+++ src/main/java/com/journal/discipline/tracker/Model/User.java
@@ -4,6 +4,11 @@ import java.time.LocalDateTime;
 
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,20 +27,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID Id;
-
     private String username;
-
     private String password;
-
     private String email;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
 
+
     private int streakCount;
-
     private int longestStreak;
-
     private double avgDisciplineScore;
 
     /*When the other models are created
