77486086dfc44938250c111d205b648443ca6d97
”EvanO3” <evan.oni@hotmail.com>
2025-10-25 22:24:40 -0400

    implemntation of retrieve user method

--- src/main/java/com/journal/discipline/tracker/Service/UserServiceImpl.java
+++ src/main/java/com/journal/discipline/tracker/Service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.journal.discipline.tracker.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.journal.discipline.tracker.DTOs.UserDTO;
+import com.journal.discipline.tracker.DTOs.UserResponse;
 import com.journal.discipline.tracker.Exceptions.ApiException;
 import com.journal.discipline.tracker.Model.User;
 import com.journal.discipline.tracker.Repository.UserRespository;
@@ -40,12 +42,25 @@ private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.cla
         return modelMapper.map(newUser, UserDTO.class);
         
     }
+    /*TODO:
+     * Get User information  from db by id
+     */
+    @Override
+    public UserResponse retrieveUser(UUID userId) {
+        User user = userRespository.findById(userId)
+        .orElseThrow(()-> new ApiException("User with Id : " + userId + " Does not exist"));
+
+        //set the user response information and return the user response
+        UserResponse userResponse = new UserResponse();
+        userResponse.setUsername(user.getUsername());
+        userResponse.setStreakCount(user.getStreakCount());
+        userResponse.setAvgDisciplineScore(user.getAvgDisciplineScore());
+        userResponse.setLongestStreak(user.getLongestStreak());
+        return userResponse;
+    }
     
 
 
-     /*TODO:
-      * Get User information  from db by id
-      */
 
        /*TODO:
      * Editing user information i.e username
