739b571fecaad8ee6f7e5ecd7dfe814f47eb1143
”EvanO3” <evan.oni@hotmail.com>
2025-10-26 12:35:37 -0400

    added Update username method for user

--- src/main/java/com/journal/discipline/tracker/Service/UserService.java
+++ src/main/java/com/journal/discipline/tracker/Service/UserService.java
@@ -9,4 +9,6 @@ public interface UserService {
     
     UserDTO createUser(UserDTO user);
     UserResponse retrieveUser(UUID userId);
+    UserDTO deleteUser(UUID userId);
+    UserDTO updateUsername(UserDTO user, UUID userId);
 }
