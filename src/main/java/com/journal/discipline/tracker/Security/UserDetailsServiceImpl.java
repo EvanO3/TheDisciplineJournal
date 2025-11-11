487bbbebea87b2f92eec0374b697869d365ecc46
”EvanO3” <evan.oni@hotmail.com>
2025-11-11 12:04:21 -0500

    filterchain

---
+++ src/main/java/com/journal/discipline/tracker/Security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.journal.discipline.tracker.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.journal.discipline.tracker.DTOs.APIResponse;
+import com.journal.discipline.tracker.Exceptions.ApiException;
+import com.journal.discipline.tracker.Model.User;
+import com.journal.discipline.tracker.Repository.UserRespository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+
+    private UserRespository userRespository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+     User user =userRespository.findByUsername(username).orElseThrow(() -> new ApiException("Failed to retrieve user with username: " + username));
+     return new UserDetailsImpl(user);
+    }
+    
+}
