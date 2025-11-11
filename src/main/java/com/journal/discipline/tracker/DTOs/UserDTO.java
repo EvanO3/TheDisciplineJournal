8da1907d5eea9587b885fd2f7fe3d8da38a1a914
”EvanO3” <evan.oni@hotmail.com>
2025-11-11 11:55:07 -0500

    added sec regex

--- src/main/java/com/journal/discipline/tracker/DTOs/UserDTO.java
+++ src/main/java/com/journal/discipline/tracker/DTOs/UserDTO.java
@@ -26,6 +26,8 @@ public class UserDTO {
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 
+    @Pattern(regexp ="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,30}$",
+    message = "Password must be at least 8 characters long, include 1 uppercase, 1 lowercase, 1 number, 1 special character, and contain no spaces")
     private String password;
 
     private String email;
