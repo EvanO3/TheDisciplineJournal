320d1ed419fc58ba2ae6558495a106ae4249fae3
”EvanO3” <evan.oni@hotmail.com>
2025-11-16 23:27:48 -0500

    refactor

--- src/main/java/com/journal/discipline/tracker/Model/Habit.java
+++ src/main/java/com/journal/discipline/tracker/Model/Habit.java
@@ -4,25 +4,34 @@ import java.util.UUID;
 
 import com.journal.discipline.tracker.Enums.HabitType;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Table(name="habit")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Habit {
     
     @Column(name="habit_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID habitId;
-    
+
+   
     private String habitName;
     @Column(name = "description")
     private String desc;
@@ -32,5 +41,12 @@ public class Habit {
 
     private String colour;
 
+
+    @ManyToOne
+    @JoinColumn(name ="user_id")
+    private User user;
+
+
+    
 
 }
