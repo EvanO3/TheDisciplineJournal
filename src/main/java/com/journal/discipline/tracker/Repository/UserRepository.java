26df17024bfca0a6ac76b6f08b5181bcc3296e26
”EvanO3” <evan.oni@hotmail.com>
2025-11-24 22:53:00 -0500

    new user repo methods

--- src/main/java/com/journal/discipline/tracker/Repository/UserRepository.java
+++ src/main/java/com/journal/discipline/tracker/Repository/UserRepository.java
@@ -4,14 +4,19 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.journal.discipline.tracker.Model.JournalEntry;
+import com.journal.discipline.tracker.DTOs.StreakData;
+
 import com.journal.discipline.tracker.Model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
- 
+    
+    @Query(value="SELECT Longest_Streak, Streak_count , Avg_Discipline_Score from USERS where id = :userId", nativeQuery =true)
+    StreakData findStreakDataById(@Param("userId") UUID userId);
         
 }
