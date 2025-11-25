b1203798085ee2a733d5e01e4240821494053cb1
”EvanO3” <evan.oni@hotmail.com>
2025-11-24 22:20:39 -0500

    HabitLogResponse

--- src/main/java/com/journal/discipline/tracker/DTOs/HabitLogResponse.java
+++ src/main/java/com/journal/discipline/tracker/DTOs/HabitLogResponse.java
@@ -1,5 +1,21 @@
 package com.journal.discipline.tracker.DTOs;
 
+import java.time.LocalDate;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HabitLogResponse {
-    
+    private  HabitDTO habit;
+    private boolean completionStatus;
+    private LocalDate logDate;
 }
+
+
+
