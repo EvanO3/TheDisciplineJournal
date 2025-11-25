f1a6cf91fee6060d6dc72b72a07233ad84cb4771
”EvanO3” <evan.oni@hotmail.com>
2025-11-24 22:16:51 -0500

    refactor to use jwtUtils

--- src/main/java/com/journal/discipline/tracker/Controller/UserController.java
+++ src/main/java/com/journal/discipline/tracker/Controller/UserController.java
@@ -5,27 +5,31 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.journal.discipline.tracker.DTOs.StreakData;
+import com.journal.discipline.tracker.DTOs.SummaryDTO;
 import com.journal.discipline.tracker.DTOs.UserDTO;
 import com.journal.discipline.tracker.DTOs.UserResponse;
 import com.journal.discipline.tracker.Exceptions.ApiException;
 import com.journal.discipline.tracker.Jwt.JwtUtils;
-import com.journal.discipline.tracker.Model.User;
+
 import com.journal.discipline.tracker.Service.UserService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+
+
 
 @RestController
 @RequestMapping("/api")
@@ -37,44 +41,50 @@ private static final Logger logger = LoggerFactory.getLogger(UserController.clas
     @Autowired
     JwtUtils jwtUtils;
 
-    @GetMapping("/user")
+    @GetMapping("/public/user")
     public ResponseEntity<UserResponse> retrieveUser(HttpServletRequest request ){
-        UUID userId = provideUserIdFromRequest(request);
+        UUID userId = jwtUtils.provideUserIdFromRequest(request);
 
         UserResponse user = userService.retrieveUser(userId);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 
-     @DeleteMapping("/user")
+     @DeleteMapping("/public/user")
     public ResponseEntity<UserDTO> deleteUser( HttpServletRequest request){
         
-        UUID userId = provideUserIdFromRequest(request);
+        UUID userId = jwtUtils.provideUserIdFromRequest(request);
         UserDTO user = userService.deleteUser(userId);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @PutMapping("/user")
+    @PutMapping("/public/user")
     public ResponseEntity<UserDTO> updateUsername( @Valid @RequestBody UserDTO userDTO, HttpServletRequest request){
-        UUID userId = provideUserIdFromRequest(request);
+        UUID userId = jwtUtils.provideUserIdFromRequest(request);
         UserDTO user = userService.updateUsername(userDTO,userId);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 
+    @GetMapping("/public/user/summary")
+    public ResponseEntity<SummaryDTO> userDailySummary(HttpServletRequest request){
+        UUID userId = jwtUtils.provideUserIdFromRequest(request);
 
-
-
-
-    /*Helper function for user Id */
-
-    private UUID provideUserIdFromRequest(HttpServletRequest request){
-        String token = jwtUtils.getJwtTokenFromHeader(request);
-        String extractedUserId = jwtUtils.getUserIdFromJwt(token);
-
-        if(extractedUserId.isEmpty() || extractedUserId ==null){
-            throw new ApiException("Failed to retrieve user Id");
-        }
-        return UUID.fromString(extractedUserId);
+        SummaryDTO userSummary = userService.getUserDailySummary(userId);
+        return new ResponseEntity<>(userSummary, HttpStatus.OK);
     }
+
+
+    @PatchMapping("/public/user")
+    public ResponseEntity<StreakData> updateUserStreak(HttpServletRequest request){
+        UUID usersId = jwtUtils.provideUserIdFromRequest(request);
+
+        StreakData userStreakData = userService.updateUserStreakData(usersId);
+        return new ResponseEntity<>(userStreakData, HttpStatus.OK);
+        
+    }
+
+
+
+
 }
